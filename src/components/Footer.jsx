54f652b53e9d41a9dfce0e493280b1caa6819311
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-10-24 23:06:45 +0500

    Footer links corected
    Changes to be committed:
    	modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,44 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
+  const navItems = [
+    { to: "/", name: "Home" },
+    { to: "about", name: "About" },
+    { to: "menu", name: "Menu" },
+    { to: "booking-page", name: "Booking" },
+    { to: "reservations", name: "Reservations" },
+    { to: "order-online", name: "Order" },
+  ];
   return (
     <div className="grids col-span-full  py-4 h-60 ">
-    <footer className=" grids-section-width row-satart-3   py-4 px-4  flex items-start justify-between flex-row ">
-      <img
-        className=" h-36 w-28 md:h-40 md:w-32 lg:h-44 lg:w-32  hidden md:block object-cover  border-2 border-white  rounded-md mx-2"
-        src="../../Mario and Adrian b 2.jpg"
-        alt="Mario and Adrian"
-      />
-      <div className="  h-full flex flex-row justify-center gap-[12vw] lg:pr-12 items-start font-karla ">
-        <ul>
-          <li> <a href="">Home </a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">menu</a></li>
-          <li><a href="#">reservations</a></li>
-          <li><a href="#">order online</a></li>
-          <li><a href="#">login</a></li>
+      <footer className=" grids-section-width row-satart-3   py-4 px-4  flex items-start justify-between flex-row ">
+        <img
+          className=" h-36 w-28 md:h-40 md:w-32 lg:h-44 lg:w-32  hidden md:block object-cover  border-2 border-white  rounded-md mx-2"
+          src="../../Mario and Adrian b 2.jpg"
+          alt="Mario and Adrian"
+        />
+        <div className="  h-full flex flex-row justify-center gap-[12vw] lg:pr-12 items-start font-karla ">
+          <ul>
+            <li className=" flex flex-col text-[16px] md:text-[16.5px]">
+              {navItems.map((item) => (
+                <Link to={item.to} key={item.to}>
+                  {item.name}
+                </Link>
+              ))}
+            </li>
           </ul>
-        <ul>
-          <li>link 2</li>
-          <li>link 2</li>
-          <li>link 2</li>
-        </ul>
-        <ul>
-          <li>link 3</li>
-          <li>link 3</li>
-          <li>link 3</li>
-        </ul>
-      </div>
-    </footer>
+          <ul>
+            <li>link 2</li>
+            <li>link 2</li>
+            <li>link 2</li>
+          </ul>
+          <ul>
+            <li>link 3</li>
+            <li>link 3</li>
+            <li>link 3</li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 };
