dbf86b4cc72e353a667cb547da71d1031f9cd315
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-10-29 22:28:20 +0500

    added scroll-snap-x to Specials Section Changes to be committed: 	modified:   src/components/Main/MainMobile.jsx 	modified:   src/components/Main/SpecialsSectionForMobile.jsx 	modified:   src/components/cards/MenuCardForMobile.jsx 	modified:   src/components/forms/BookingForm.jsx

--- src/components/cards/MenuCardForMobile.jsx
+++ src/components/cards/MenuCardForMobile.jsx
@@ -1,24 +1,28 @@
 /* eslint-disable react/prop-types */
 import { Button } from "@/components/ui/button";
 
-const MenuCardForMobile = ({data}) => {
+const MenuCardForMobile = ({ data }) => {
   return (
-    //   main component
-    <div className=" relative rounded-xl border bg-card text-card-foreground shadow w-56 h-56  overflow-hidden items-center ">
-      <img
-        className="min-w-full h-full object-cover"
-        src={data.img}
-        alt={data.name}
-      />
-      {/* Footer */}
-      <div className=" absolute w-[13rem] mx-2 bottom-2  rounded-2xl overflow-hidden  h-12 flex flex-row justify-between items-center px-3 py-2 bg-background/95  supports-[backdrop-filter]:bg-background/60">
-        <p className="font-semibold leading-none tracking-tight">{data.name}</p>
-        <Button
-          variant="outline"
-          className="rounded-2xl font-karla text-red-500 text-lg backdrop-blur"
-        >
-          $ {data.price}
-        </Button>
+    <div className=" min-w-fit w-fit h-fit min-h-fit snap-center">
+      {/* // main component */}
+      <div className="relative rounded-xl border bg-card text-card-foreground shadow w-56 h-56  overflow-hidden items-center ">
+        <img
+          className="min-w-full h-full object-cover"
+          src={data.img}
+          alt={data.name}
+        />
+        {/* Footer */}
+        <div className=" absolute w-[13rem] mx-2 bottom-2  rounded-2xl overflow-hidden  h-12 flex flex-row justify-between items-center px-3 py-2 bg-background/95  supports-[backdrop-filter]:bg-background/60">
+          <p className="font-semibold leading-none tracking-tight">
+            {data.name}
+          </p>
+          <Button
+            variant="outline"
+            className="rounded-2xl font-karla text-red-500 text-lg backdrop-blur"
+          >
+            $ {data.price}
+          </Button>
+        </div>
       </div>
     </div>
   );
