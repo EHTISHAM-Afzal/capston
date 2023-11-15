1b8b5f910a0faf731fe74cae2208990c926895a9
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-15 11:13:13 +0500

    modified:   src/components/cards/MenuCardForMobile.jsx 	modified:   src/pages/Profile.jsx 	deleted:    vercel.json

--- src/components/cards/MenuCardForMobile.jsx
+++ src/components/cards/MenuCardForMobile.jsx
@@ -28,7 +28,7 @@ const MenuCardForMobile = ({ dish }) => {
           <Skeleton className="min-w-full h-full" />
         )}
         {/* Footer */}
-        <div className=" absolute pl-4 w-[95%] mx-2 bottom-2 rounded-2xl h-12 flex flex-row justify-between items-center px-3 py-2 bg-background/95  supports-[backdrop-filter]:bg-background/75">
+        <div className=" absolute pl-4 w-[95%] mx-2 bottom-2 rounded-xl h-12 flex flex-row justify-between items-center px-3 py-2 border text-black  dark:border-neutral-800 dark:bg-black/70 dark:text-white  supports-[backdrop-filter]:bg-background/75">
           <span className=" space-y-1">
             <p className="font-semibold leading-none tracking-tight">
               {dish.name}
@@ -36,8 +36,8 @@ const MenuCardForMobile = ({ dish }) => {
             <p className="text-sm ">{dish.description.substring(0, 30)}</p>
           </span>
           <Button
-            variant="outline"
-            className="rounded-2xl font-karla text-lg bg-transparent backdrop-blur"
+          variant="outline"
+            className="rounded-xl text-2xl font-karla backdrop:blur-lg border-black dark:border-white "
           >
             $ {dish.price}
           </Button>
