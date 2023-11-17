9bdf78d02e27cee219863129c793c6a25bced04b
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-17 21:02:23 +0500

    modified:   src/components/Sheets/CartSheet.jsx 	modified:   src/index.css

--- src/components/Sheets/CartSheet.jsx
+++ src/components/Sheets/CartSheet.jsx
@@ -10,78 +10,67 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import OpenCartButton from "../smallComp/OpenCartButton";
-import {useSelector } from "react-redux";
-
-function openFullscreen(elem) {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) {
-    /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
+import { useSelector } from "react-redux";
 
 const CartSheet = () => {
   const totalIntemsQuantitiesInCart = useSelector((state) =>
     state.cart.reduce((acc, item) => acc + item.quantity, 0)
   );
   const cartState = useSelector((state) => state.cart);
-  const total = useSelector((state) => state.cart.reduce((acc, item) => acc + item.price * item.quantity, 0));
+  const total = useSelector((state) =>
+    state.cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  );
   const taxes = total * 0.1;
   const totalPrice = total + taxes;
   return (
     <div>
       <Sheet>
-        <SheetTrigger
-          onClick={() => {
-            if (window.innerWidth < 600) {
-              openFullscreen(document.documentElement);
-            }
-          }}
-        >
+        <SheetTrigger>
           <OpenCartButton quantity={totalIntemsQuantitiesInCart} />
           <span className="sr-only ">Open Cart</span>
         </SheetTrigger>
-        <SheetContent className="w-full sm:w-96">
+        <SheetContent className="w-full sm:w-96 max-h-screen h-screen">
           <SheetHeader className="mb-4">
             <SheetTitle>My Cart</SheetTitle>
           </SheetHeader>
-          <ScrollArea className="h-[65vh] overflow-hidden">
-            {cartState.map((item) => (
-              <CartItemCard key={item._id} dish={item} />
-            ))}
-          </ScrollArea>
-          <div className="w-full h-full space-y-2 flex flex-col ">
-            <div className="flex justify-between">
-              <h2>Taxes</h2>
-              <h2>
-                <span>$ </span>{taxes <= 9 ? 0 : null}{taxes}
-              </h2>
+          <Separator />
+          {/* Contents */}
+          <div className="w-full h-[95%] grid grid-flow-row grid-rows-[1fr,_10rem]">
+            <ScrollArea className="h-full w-full">
+              {cartState.map((item) => (
+                <CartItemCard key={item._id} dish={item} />
+              ))}
+            </ScrollArea>
+            <div className=" w-full h-full space-y-2 flex flex-col right-1 ">
+              <div className="flex justify-between">
+                <h2>Taxes</h2>
+                <h2>
+                  <span>$ </span>
+                  {taxes <= 9 ? 0 : null}
+                  {taxes.toString().substring(0, 4)}
+                </h2>
+              </div>
+              <Separator />
+              <div className="flex justify-between">
+                <h2>Shipping</h2>
+                <h2>
+                  <span>$ </span>Free
+                </h2>
+              </div>
+              <Separator />
+              <div className="flex justify-between">
+                <h2>Total</h2>
+                <h2>
+                  <span>$ </span>
+                  {totalPrice <= 9 ? 0 : null}
+                  {totalPrice}
+                </h2>
+              </div>
+              <Separator />
+              <Button size="lg" className="min-h-[2rem]">
+                Proceed to Checkout
+              </Button>
             </div>
-            <Separator />
-            <div className="flex justify-between">
-              <h2>Shipping</h2>
-              <h2>
-                <span>$ </span>Free
-              </h2>
-            </div>
-            <Separator />
-            <div className="flex justify-between">
-              <h2>Total</h2>
-              <h2>
-                <span>$ </span>{totalPrice <= 9 ? 0 : null}{totalPrice}
-              </h2>
-            </div>
-            <Separator />
-            <Button size="lg" className="min-h-[2rem]">
-              Proceed to Checkout
-            </Button>
           </div>
         </SheetContent>
       </Sheet>
