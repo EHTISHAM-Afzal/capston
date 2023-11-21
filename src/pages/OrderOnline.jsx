3e92ace3c04a7447944669878dfaa2d629fbe362
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-21 17:14:40 +0500

    redirected to redirectPage
    	modified:   src/pages/OrderOnline.jsx
    	modified:   src/pages/Reservations.jsx

--- src/pages/OrderOnline.jsx
+++ src/pages/OrderOnline.jsx
@@ -1,30 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import Spinner from "../components/smallComp/Spinner";
-import { useGetTableByIdQuery } from "../features/BookingTables/BookingTableSlice";
+import { useEffect } from "react";
 
 const OrderOnline = () => {
-  const {
-    data: table,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = useGetTableByIdQuery("654afde601fc148e0a47b050");
-  
-  let content;
-
-  if (isSuccess) {
-    content = JSON.stringify(table, null , 2);
-  } else if (isLoading) {
-    content = <Spinner />;
-  } else if (isError) {
-    content = <p>{error.message}</p>;
-  } else {
-    content = <p>No data</p>;
-  }
-
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("underconstruction?page=OrderOnline");
+  }, []);
   return (
     <section className=" flex flex-col justify-center items-center ">
-      {content}
+      <Spinner />
     </section>
   );
 };
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/smallComp/Spinner";
+import { useEffect } from "react";
 
 const Reservations = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+     navigate("underconstruction?page=Reservations");
+  }, [])
+  
   return (
     <section className="flex-col justify-center items-center my-6 w-full ">
-      <h1 className="text-4xl font-bold text-center mb-8">Reservations is under-construction</h1>
-      <img className="w-11/12" src="../../page-under-construction.svg" alt="Under-construction" />
+      <h1 className="text-4xl font-bold text-center mb-8">
+        Reservations is under-construction
+      </h1>
+      <Spinner />
     </section>
   );
 };
