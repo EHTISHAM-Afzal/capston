36a749aed026dce524f76c519a70c7b9e10aa910
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-12-04 16:31:29 +0500

    added server is gone to sleep indication Changes to be committed: 	modified:   package.json 	modified:   src/components/cards/MenuCardSkeletonForDektop.jsx 	modified:   src/components/cards/MenuCardSkeletonForMobile.jsx 	modified:   src/pages/Reservations.jsx 	modified:   yarn.lock

--- src/components/cards/MenuCardSkeletonForDektop.jsx
+++ src/components/cards/MenuCardSkeletonForDektop.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 const MenuCardSkeletonForDektop = () => {
+  const [content, setContent] = useState(undefined);
+  setTimeout(() => {
+    setContent(
+      <h2 className="w-full text-lg">
+        Its look like the server is gone to sleep please wait untill server
+        responce
+      </h2>
+    );
+  }, 10000);
+
   return (
     <div className="w-[16rem] h-[24rem] bg-primary/10 mx-1 my-2 border overflow-hidden rounded-xl">
       <div className="w-full h-[40%] bg-black/20 dark:bg-white/10 border " />
@@ -10,7 +22,11 @@ const MenuCardSkeletonForDektop = () => {
           <div className=" w-10 h-full bg-black/20 dark:bg-white/10" />
         </div>
         {/* Main */}
-        <div className=" w-full h-44 bg-black/20 dark:bg-white/10" />
+        {content ? (
+          content
+        ) : (
+          <div className=" w-full h-44 bg-black/20 dark:bg-white/10" />
+        )}
       </div>
     </div>
   );
--- src/components/cards/MenuCardSkeletonForMobile.jsx
+++ src/components/cards/MenuCardSkeletonForMobile.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 const MenuCardSkeletonForMobile = () => {
+  const [content, setContent] = useState(undefined);
+
+  setTimeout(() => {
+    setTimeout(() => {
+      setContent(
+        <h2 className="absolute bottom-2 left-[2%]  w-[96%] text-base p-1 rounded-lg border animate-pulse">
+          Its look like the server is gone to sleep please wait untill server
+          responce
+        </h2>
+      );
+    }, 10000);
+  });
   return (
-    <div className=" w-full h-full snap-start">
+    <div className=" w-80 h-44 snap-start">
       <div className="relative rounded-xl border-2 w-full h-full bg-black/20 dark:bg-white/10 overflow-hidden ">
-        <div className="absolute bottom-2 left-[2%]  w-[96%] rounded-lg border  h-12 bg-black/20 dark:bg-white/10 animate-pulse" />
+        {content ? (
+          content
+        ) : (
+          <div className="absolute bottom-2 left-[2%]  w-[96%] rounded-lg border  h-12 bg-black/20 dark:bg-white/10 animate-pulse" />
+        )}
       </div>
     </div>
   );
